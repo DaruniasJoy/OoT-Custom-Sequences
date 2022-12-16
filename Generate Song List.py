e7d74138bc278591dd419f9bf469ea665dba8c43
Cuphat <matthew@cuphat.com>
2022-12-16 03:06:41 -0500

    Add `groups_list.json` generation to `Generate Song List.py`
    This adds generation of a `groups_list.json` file when generating the song list which contains all the groups embedded in the .meta files, as well as a list of ungrouped songs at the bottom.
    
    Additionally, fixes text encoding issues in the song list generation.

--- Generate Song List.py
+++ Generate Song List.py
@@ -1,5 +1,7 @@
+import json
 import os
 import traceback
+
 
 def generate_song_list():
     if not os.path.exists('data/Music'):
@@ -9,19 +11,27 @@ def generate_song_list():
     songs = os.walk('data/Music')
 
     song_list = ""
+    groups = {'_$ungrouped_songs$_': []}
 
     for root, dirs, files in songs:
         level = root.replace('data/Music', '').count(os.sep)
-        indent = ' ' * 4 * (level)
-        song_list += '{}- {}\r\n'.format(indent, os.path.basename(root))
+        indent = ' ' * 4 * level
+        song_list += f'{indent}- {os.path.basename(root)}\n'
         subindent = ' ' * 4 * (level + 1)
         for f in files:
-            if '.meta' not in f:
+            if not f.endswith('.meta'):
                 continue
-            song_list += '{} key: {}\r\n'.format(subindent, song_name_from(root, f))
+            song_name, instrument_set, seq_type, seq_groups = parse_meta_file(root, f)
+            song_list += f'{subindent}key: {song_name}\n'
+            for group in seq_groups:
+                if group not in groups:
+                    groups[group] = []
+                groups[group].append(song_name)
+            if not seq_groups:
+                groups['_$ungrouped_songs$_'].append(song_name)
 
     try:
-        create_song_list_file(song_list)
+        create_song_list_file(song_list, groups)
     except Exception as e:
         print("An error occured creating song list")
         print(traceback.format_exc())
@@ -29,24 +39,45 @@ def generate_song_list():
 
     return True
 
-def create_song_list_file(song_list):
+
+def create_song_list_file(song_list, groups):
     with open('song_list.txt', 'w', encoding='utf-8') as song_list_file:
         song_list_file.write(song_list)
 
-def song_name_from(root, file):
+    if groups:
+        with open('groups_list.json', 'w', encoding='utf-8') as groups_file:
+            json_output = {'groups': groups, 'ungrouped_songs': groups.pop('_$ungrouped_songs$_')}
+            if not json_output['ungrouped_songs']:
+                del json_output['ungrouped_songs']
+            json.dump(json_output, groups_file, indent=4, ensure_ascii=False, sort_keys=True)
+
+
+def parse_meta_file(root, file):
     path = root + "/" + file
-    with open(path, 'r') as meta_file:
-        return meta_file.readline().rstrip()
+    with open(path, 'r', encoding='utf-8') as meta_file:
+        lines = meta_file.readlines()
+
+    # Strip newline(s)
+    lines = [line.rstrip() for line in lines]
+    song_name = lines[0]
+    instrument_set = lines[1]
+    seq_type = lines[2] if len(lines) >= 3 else 'bgm'
+    groups = [g.strip() for g in lines[3].split(',')] if len(lines) >= 4 else []
+
+    return song_name, instrument_set, seq_type, groups
 
 
-if __name__ == '__main__':
+def main():
     song_list_generated = generate_song_list()
-    
+
     if song_list_generated:
         print("song_list.txt file successfully created!")
         print("Have fun customizing your randomizer! :)")
         print("")
     else:
         print("An error occured generating your song list")
-    input("Press enter to quit")
+    input("Press enter to quit.")
 
+
+if __name__ == '__main__':
+    main()
