ed2da44c07d03281f0b3098d4e4f063df90e7e0d
Cristóbal Peñaloza <cristobal.tolin@gmail.com>
2024-05-03 11:50:01 -0400

    Update make_title_screen_files.py to current format (ootrs)

--- make_title_screen_files.py
+++ make_title_screen_files.py
@@ -3,26 +3,34 @@ import json
 import os
 import subprocess
 import sys
+import zipfile
 
 
 # Specify the full path to your rando directory if necessary.
-# Defaults to 3 directories up from current (e.g. this repo is in data/Music/OoT-Custom-Sequences inside rando)
-RANDO_DIRECTORY = "../../.."
+# Defaults to the same directory as current.
+RANDO_DIRECTORY = "."
 
 
 def parse_meta_file(root: str, file: str) -> tuple[str, str, str, list[str]]:
     path = root + "/" + file
-    with open(path, 'r', encoding='utf-8') as meta_file:
-        lines = meta_file.readlines()
+    archive = zipfile.ZipFile(path, 'r')
 
-    # Strip newline(s)
-    lines = [line.rstrip() for line in lines]
-    song_name = lines[0]
-    instrument_set = lines[1]
-    seq_type = lines[2] if len(lines) >= 3 else 'bgm'
-    groups = [g.strip() for g in lines[3].split(',')] if len(lines) >= 4 else []
+    for name in archive.namelist():
+        if not name.endswith('.meta'):
+            continue
 
-    return song_name, instrument_set, seq_type, groups
+        with archive.open(name) as meta_file:
+            lines = meta_file.readlines()
+
+            # Strip newline(s)
+            lines = [line.decode('utf8').rstrip() for line in lines]
+            song_name = lines[0]
+            instrument_set = lines[1]
+            seq_type = lines[2] if len(lines) >= 3 else 'bgm'
+            groups = [g.strip() for g in lines[3].split(',')] if len(lines) >= 4 else []
+
+            return song_name, instrument_set, seq_type, groups
+    
 
 
 def main():
@@ -38,24 +46,28 @@ def main():
     for root, dirs, files in songs:
         root = root.replace('\\', '/')
         for f in files:
-            if not f.endswith('.meta'):
+            print(f'Scanning file {f}')
+            if not f.endswith('.ootrs'):
                 continue
-            seq_path = os.path.join(root, f"{f[:-5]}.seq")
-            if not os.path.isfile(seq_path):
-                print(f'Sequence file "{seq_path}" does not exist.')
+
+            ootrs_path = os.path.join(root, f)
+            if not os.path.isfile(ootrs_path):
+                print(f'OOTRS file "{ootrs_path}" does not exist.')
                 continue
 
             song_name, instrument_set, seq_type, seq_groups = parse_meta_file(root, f)
+            print(f'Detected data: {song_name} {instrument_set} {seq_type} {seq_groups}')
 
             output_folder = f"{root.replace('data/Music', 'Output')}"
-            output_base = f"{output_folder}/{f[:-5]}"
+            output_base = f"{output_folder}/{f[:-6]}"
+            print(f'output_base: {output_base}')
             cosmetic_plando_filename = f'{output_base}_cosmetic.json'
             settings_filename = f'{output_base}_settings.json'
             os.makedirs(output_folder, exist_ok=True)
 
             if os.path.isfile(f"{output_base}.z64"):
                 rom_modified = os.path.getmtime(f"{output_base}.z64")
-                seq_modified = os.path.getmtime(seq_path)
+                seq_modified = os.path.getmtime(ootrs_path)
                 meta_modified = os.path.getmtime(os.path.join(root, f))
                 if rom_modified > seq_modified and rom_modified > meta_modified:
                     continue
@@ -76,10 +88,11 @@ def main():
                 "enable_cosmetic_file": True,
                 "cosmetic_file": os.path.abspath(cosmetic_plando_filename),
                 "output_dir": os.path.abspath(output_folder),
-                "output_file": f[:-5],
+                "output_file": f[:-6],
                 "create_compressed_rom": True,
                 "create_spoiler": False,
                 "create_cosmetics_log": False,
+                "logic_rules": "none",
                 "starting_items": {
                     "Ocarina": 1,
                     "Zeldas Lullaby": 1,
@@ -88,7 +101,7 @@ def main():
             with open(f'{settings_filename}', 'w', encoding='utf-8', newline='') as file:
                 json.dump(rando_settings, file, indent=4)
 
-            print(f'Generating ROM for "{output_base}".')
+            print(f'Generating ROM for "{output_base}".ootrs')
             subprocess.run([sys.executable, rando_script_path, "--settings", os.path.abspath(settings_filename)], stdout=subprocess.DEVNULL, shell=False, check=True)
             os.remove(settings_filename)
             os.remove(cosmetic_plando_filename)
